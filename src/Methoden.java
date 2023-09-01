da62b9fc74f29cf25acd45fa930803931fc8f655
WaldyWhite <w.weis@hotmail.com>
2023-09-01 17:53:47 +0200

    first commit

---
+++ src/Methoden.java
@@ -0,0 +1,11 @@
+public class Methoden {
+    public static void main(String[] args) {
+        int x = arithmetic(25, 30);
+        System.out.println(x);
+    }
+    static int arithmetic(int a, int b) {
+        return a + b;
+    }
+
+
+}
